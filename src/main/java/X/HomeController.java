6addaa93f3b9749c8eb6b0a2da72cab8e8d6f458
rhakobyan <hakobyanruben2000@gmail.com>
2019-07-13 00:32:12 +0100

    Added some 'design'

--- src/main/java/X/HomeController.java
+++ src/main/java/X/HomeController.java
@@ -10,9 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+     /*  @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
-    }
+    }*/
 
 }
